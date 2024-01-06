6ccfcc41ab44d0296c241d9a688de1e90d0d3e7e
amineladraa1 <amineladraa67@gmail.com>
2024-01-06 02:57:01 -0800

    task 6

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,6 +4,7 @@ from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
+'''The blueprint for the AirBnB clone API.'''
 
 from .index import *
+from .states import *
