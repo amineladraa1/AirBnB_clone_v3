4059aca8dff24be24bb8fb8b4fd2e5cdc525206a
amineladraa1 <amineladraa67@gmail.com>
2024-01-07 02:57:11 -0800

    add CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,13 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 import os
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.register_blueprint(app_views)
-
+CORS(app, resources={'/*': {'origins': app_host}})
 
 @app.teardown_appcontext
 def call_storage(exception):
@@ -21,4 +23,4 @@ if __name__ == "__main__":
     HBNB_API_PORT = os.getenv('HBNB_API_PORT')
     host = HBNB_API_HOST if HBNB_API_HOST else '0.0.0.0'
     port = HBNB_API_PORT if HBNB_API_PORT else 5000
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, threaded=True)
